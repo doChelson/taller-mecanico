70e977a8e1fb972bfd42ef56b45c155569c47ae3
doChelson <marceshausha@gmail.com>
2026-06-30 02:55:31 -0400

    Update OrdenTrabajoDTO.java

--- backend/src/main/java/com/example/signin/dto/OrdenTrabajoDTO.java
+++ backend/src/main/java/com/example/signin/dto/OrdenTrabajoDTO.java
@@ -8,27 +8,11 @@ public class OrdenTrabajoDTO {
 
     public OrdenTrabajoDTO() {}
 
-    public String getEstado() {
-        return estado;
-    }
+    public String getEstado() { return estado; }
+    public String getDiagnosticoPreliminar() { return diagnosticoPreliminar; }
+    public Integer getVehiculoId() { return vehiculoId; }
 
-    public String getDiagnosticoPreliminar() {
-        return diagnosticoPreliminar;
-    }
-
-    public Integer getVehiculoId() {
-        return vehiculoId;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public void setDiagnosticoPreliminar(String diagnosticoPreliminar) {
-        this.diagnosticoPreliminar = diagnosticoPreliminar;
-    }
-
-    public void setVehiculoId(Integer vehiculoId) {
-        this.vehiculoId = vehiculoId;
-    }
+    public void setEstado(String estado) { this.estado = estado; }
+    public void setDiagnosticoPreliminar(String diagnosticoPreliminar) { this.diagnosticoPreliminar = diagnosticoPreliminar; }
+    public void setVehiculoId(Integer vehiculoId) { this.vehiculoId = vehiculoId; }
 }
